9fed4a42c8a5a1efd88d74bbc41344b1caa328d2
Anthony Romero <iaromerod@unac.edu.co>
2021-03-23 16:14:23 -0500

    Actualización

---
+++ Operadores.py
@@ -0,0 +1,32 @@
+
+""" Aritmeticos :
+#    suma, resta, multiplicacion, division, exponencial, residuo, division entera
+
+#comparacion (return bool):
+#    igual que, mayor que, menor que, diferente de, mayor o igual, menor o igual
+
+#logicos :
+#   si (and), o (or), negación (not)
+
+#asignacion :
+#    igual, incremento, decremento, multiplicado, dividido, modulo, div. entera, exponente
+
+#especiales
+#    esta (is), no está (is not), en (in), no en (not in)"""
+
+def main():
+
+    num1= 2
+    num2 = 4
+
+    print(num1 > num2 or not num1 != num2)
+    print(num1 < num2)
+    print(num1 == num2)
+    print(num1 != num2)
+    print(num1 >= num2)
+    print(num1 <= num2)
+
+
+main()
+
+# principales funciones y características de las cadenas de texto
