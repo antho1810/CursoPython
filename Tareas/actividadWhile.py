0d7421064cd85b5e7f231ba0b198e013278ae71d
Anthony Romero <iaromerod@unac.edu.co>
2021-04-06 18:08:09 -0500

    .

--- Tareas/actividadWhile.py
+++ Tareas/actividadWhile.py
@@ -1,8 +1,9 @@
  def main():
-
- num = float(input("ingrese numeros decimales:"))
+    num1 = float(input("ingrese un numero"))
+    num2 = float(input(f"ingrese un numero mayor a {num1}"))
  
- while num > 5
-    num = float(input("por favor intruzca un numero mayo al primero"))
- print(num) 
+ while num2 > num1
+    num1 = num2
+    num2 = float(input("ingerese un numero mayor a "+ str(num2)))
+ print(num2) 
 main()
