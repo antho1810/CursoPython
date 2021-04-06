9fd186c5ba2b80c6fa05efe06a03308e888f53d2
Anthony Romero <iaromerod@unac.edu.co>
2021-04-06 16:11:23 -0500

    Tarea
    Esta tarea es de Operadores, Condicionales y Datos_Teclado.

--- Tarea.py
+++ Tarea.py
@@ -23,3 +23,35 @@ crear un programa que le pida al usuario su edad, nombre, dos números y muestre
     Edad: 
     
 '''
+def main():
+    suma = "1"
+    resta = "2"
+    multiplicación = "3"
+    division = "4"
+    exponente = "5"
+
+    nombre = str(input("Ingrese su nombre: "))
+    edad = str(input("Ingrese su edad: "))
+    V1 = int(input("Ingrese el primer valor: "))
+    V2 = int(input("Ingrese el segundo valor: "))
+    id = input("1-suma\n2-resta\n3-multiplicacion\n4-division\n5-exponente\nopcion: ")
+    print("Su resultado es: ")
+
+    if(id == suma):
+        print(V1 + V2)
+    elif(id == resta):
+        print(V1 - V2)
+    elif(id == multiplicación):
+        print(V1 * V2)
+    elif(id == division):
+        print(V1 / V2)
+    elif(id == exponente):
+        print(V1 ** V2)
+    else:
+        print("Error")
+    
+    print("Su nombre es: " + nombre)
+    print("Su edad es: "+ edad)
+
+    
+main()
